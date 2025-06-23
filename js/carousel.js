6cdc15bad6dc62b929e69b6ff9ccfd6ef10111bc
Matheus <kjillimlenz@unesc.net>
2025-06-23 11:54:58 -0300

    bugfix

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,8 @@
 window.onload = function () {
     const imagens = ["img/audi.jpg", "img/bmw.jpg", "img/civic.jpg"];
     let indexCentral = 1;
+    let intervalo;
+    let mouseSobreCarrossel = false;
 
     const updateCarrossel = () => {
         const esquerda = document.querySelector('.item.esquerda img');
@@ -8,7 +10,6 @@ window.onload = function () {
         const direita = document.querySelector('.item.direita img');
 
         if (esquerda && central && direita) {
-            // Fade-out
             [esquerda, central, direita].forEach(img => img.classList.add('fade-out'));
 
             setTimeout(() => {
@@ -17,27 +18,53 @@ window.onload = function () {
                 direita.src = imagens[(indexCentral + 1) % imagens.length];
 
                 [esquerda, central, direita].forEach(img => img.classList.remove('fade-out'));
-            }, 300);
+            }, 400);
         }
     };
 
     const avancar = () => {
         indexCentral = (indexCentral + 1) % imagens.length;
         updateCarrossel();
+        resetarIntervalo();
     };
 
     const voltar = () => {
         indexCentral = (indexCentral - 1 + imagens.length) % imagens.length;
         updateCarrossel();
+        resetarIntervalo();
     };
 
-    // Botões seta
+    const iniciarIntervalo = () => {
+        intervalo = setInterval(() => {
+            if (!mouseSobreCarrossel) {
+                avancar();
+            }
+        }, 4000);
+    };
+
+    const resetarIntervalo = () => {
+        clearInterval(intervalo);
+        iniciarIntervalo();
+    };
+
+    const carrossel = document.querySelector('.carrossel-container');
+
+    if (carrossel) {
+        carrossel.addEventListener('mouseenter', () => {
+            mouseSobreCarrossel = true;
+        });
+
+        carrossel.addEventListener('mouseleave', () => {
+            mouseSobreCarrossel = false;
+        });
+    }
+
     document.querySelector('.seta.direita')?.addEventListener('click', avancar);
     document.querySelector('.seta.esquerda')?.addEventListener('click', voltar);
 
-    // Clique nas imagens laterais
     document.querySelector('.item.direita img')?.addEventListener('click', avancar);
     document.querySelector('.item.esquerda img')?.addEventListener('click', voltar);
 
     updateCarrossel();
+    iniciarIntervalo();
 };
